c4f8b9dcba4ff6d02e8e21a788b90607b3a761d4
kishor_saha <kishorsaha650@gmail.com>
2024-05-15 00:55:42 +0600

    daynamic rout

---
+++ app/service/[id]/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const page = ({id}) => {
+
+   return (
+      <div>
+        <h2>id:{id}</h2>
+      </div>
+   );
+};
+
+export default page;
---
+++ app/service/layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const serviceLayout = ({children}) => {
+   return (
+      <div>
+         lippp
+         {children}
+      </div>
+   );
+};
+
+export default serviceLayout;
